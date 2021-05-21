6a6a3a8769ff254da248e9cbcf5413a8fdb1d2e8
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-21 18:01:46 +0200

    feat: post request for transfer complete

--- js/transfer.js
+++ js/transfer.js
@@ -1,10 +1,27 @@
-const baseUrl = "http://localhost:3000/transfers/"
-
-let submitTransfer = document.getElementById("submitTransfer").addEventListener('click', ()=> {
+let submitTransfer = document.getElementById("submitTransfer").addEventListener('click', e => {
     let recipient = document.getElementById("recipient").value;
     let reason = document.getElementById("reason").value;
     let message = document.getElementById("message").value;
     let amount = document.getElementById("amount").value;
 
-    console.log(recipient + reason + message + amount);
+    fetch('http://localhost:3000/transfers/create', {
+        method: "post",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify ({
+            "recipient": recipient,
+            "reason": reason,
+            "message": message,
+            "amount": amount
+        })
+    }) .then(result => {
+        return result.json();
+    }) .then(json => {
+        console.log(json);
+    }) . catch(err => {
+        console.log(err);
+    })
+
+    e.preventDefault();
 })
