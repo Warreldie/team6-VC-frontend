d6ad833a349713f40f82b0ac7841769a8ddf018e
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-21 17:20:54 +0200

    feat: test button click on submit transfer

---
+++ js/transfer.js
@@ -0,0 +1,7 @@
+const baseUrl = "http://localhost:3000/transfers/"
+
+let submitTransfer = document.getElementById("submitTransfer").addEventListener('click', ()=> {
+    console.log("hi");
+
+    
+})
