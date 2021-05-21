cedae6f22ef838c90eebc4261ea8ff1251a3a4c7
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-21 17:24:35 +0200

    feat: get form values on submit transfer

--- js/transfer.js
+++ js/transfer.js
@@ -1,7 +1,10 @@
 const baseUrl = "http://localhost:3000/transfers/"
 
 let submitTransfer = document.getElementById("submitTransfer").addEventListener('click', ()=> {
-    console.log("hi");
+    let recipient = document.getElementById("recipient").value;
+    let reason = document.getElementById("reason").value;
+    let message = document.getElementById("message").value;
+    let amount = document.getElementById("amount").value;
 
-    
+    console.log(recipient + reason + message + amount);
 })
