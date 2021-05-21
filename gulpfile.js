66ec872ba5cc9b5dc6d87b8a33ea56fc86306bd7
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-21 12:09:16 +0200

    feature: gulp sass + created sass2css script

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const { src, dest} = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+exports.sass2css = function (){
+    return src('./src/sass/app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./src/dist/app.css'));
+}
