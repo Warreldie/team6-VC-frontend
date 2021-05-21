ef8deaf1bbf1993e63ff7d9d4ecb2286eedd75ac
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-21 16:08:31 +0200

    fix: gulp watch sass2css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const { src, dest} = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-exports.sass2css = function (){
+sass2css = function (){
     return src('./src/sass/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./src/dist/'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('./src/dist/'));
+}
+
+exports.default = function() {
+    watch('./src/sass/**/*.scss', sass2css);
 }
