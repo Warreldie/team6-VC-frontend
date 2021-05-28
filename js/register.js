416df73f865f955643125bfbcd42353cf2ad3ba8
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-05-28 21:48:29 +0200

    feat: only submit register form when username and password filled out correctly

--- js/register.js
+++ js/register.js
@@ -2,25 +2,27 @@ let submitRegister = document.getElementById("submitRegister").addEventListener(
     let username = document.getElementById("username").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    // let repeatPassword = document.getElementById("repeatPassword").value;
+    let repeatPassword = document.getElementById("repeatPassword").value;
 
-    fetch('http://localhost:3000/users/register', {
-        method: "post",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify ({
-            "username": username,
-            "email": email,
-            "password": password
+    if (username != "" && (password != "" && password == repeatPassword)) {
+        fetch('http://localhost:3000/users/register', {
+            method: "post",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "email": email,
+                "password": password
+            })
+        }).then(result => {
+            return result.json();
+        }).then(json => {
+            console.log(json);
+        }).catch(err => {
+            console.log(err);
         })
-    }) .then(result => {
-        return result.json();
-    }) .then(json => {
-        console.log(json);
-    }) . catch(err => {
-        console.log(err);
-    })
 
-    e.preventDefault();
+        e.preventDefault();
+    }
 })
